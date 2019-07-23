d298ce8d13a984b707f3c0295662a40455afd659
Jordan Hall <jordan@hall05.co.uk>
2019-07-23 22:34:52 +0100

    Initial package

---
+++ src/Traits/OmegaSearchTrait.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace DivineOmega\LaravelOmegaSearch\Traits;
+
+use DivineOmega\OmegaSearch\OmegaSearch;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+trait OmegaSearchTrait
+{
+    /**
+     * Perform an intelligent fuzzy search, and returns a query builder filtered
+     * to the related records in descending order of relevance.
+     *
+     * @param $searchText
+     *
+     * @return Builder
+     */
+    public static function omegaSearch($searchText)
+    {
+        /** @var Model $model */
+        $model = new self();
+        $keyName = $model->getKeyName();
+
+        $search = (new OmegaSearch())
+            ->setDatabaseConnection(DB::getPdo())
+            ->setTable($model->getTable())
+            ->setPrimaryKey($keyName)
+            ->setFieldsToSearch($model->getOmegaSearchFieldsToSearch())
+            ->setConditions($model->getOmegaSearchConditions());
+
+        $results = $search->query($searchText, 100);
+
+        $ids = array_map(function ($result) {
+            return $result->id;
+        }, $results->results);
+
+        $products = self::query()
+            ->whereIn($keyName, $ids)
+            ->orderByRaw(DB::raw('FIELD('.$keyName.', '.implode(',', $ids).')'));
+
+        return $products;
+    }
+
+    /**
+     * Must return an array of the model fields (attribute) to search.
+     *
+     * @return array
+     */
+    abstract public function getOmegaSearchFieldsToSearch();
+
+    /**
+     * Must return an associative array of the search conditions.
+     *
+     * e.g ['active' => 1, 'discontinued' => 0]
+     *
+     * @return array
+     */
+    abstract public function getOmegaSearchConditions();
+}
